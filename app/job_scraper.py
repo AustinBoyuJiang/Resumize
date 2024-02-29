aa4bea2662f5ab36fe8150ec1dcbf67e25733200
Game Development Club WVSS <austinjiangboyu@gmail.com>
2024-02-29 01:29:51 -0800

    Update job_scraper.py

--- app/job_scraper.py
+++ app/job_scraper.py
@@ -5,16 +5,7 @@ headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
 params = {
     'linkedin_profile_url': 'https://www.linkedin.com/in/epic-eric/',
-    'extra': 'include',
-    'github_profile_id': 'include',
-    'facebook_profile_id': 'include',
-    'twitter_profile_id': 'include',
-    'personal_contact_number': 'include',
-    'personal_email': 'include',
-    'inferred_salary': 'include',
-    'skills': 'include',
-    'use_cache': 'if-present',
-    'fallback_to_cache': 'on-error',
+    'use_cache': 'if-recent',
 }
 response = requests.get(api_endpoint,
                         params=params,
